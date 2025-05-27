1c5db20d4def2b8d0ec115332473f86e43d710f1
Philipp Meyer <philippmeyer2003@gmail.com>
2025-05-27 18:05:06 +0200

    Minor st changes

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from utils.page_setup import set_page_layout
 
-set_page_layout(page_title="Welcome! Use the sidebar to navigate:")
+set_page_layout(
+    page_title="This is a demopage for the PyPi package ```easy-nlp-translate``` to be found at https://pypi.org/project/easy-nlp-translate/",
+)
 
-st.markdown("""
-- **Upload:** Upload and translate a new video
-- **Library:** View & play your translated videos
-""")
 
-st.image("src/images/world.jpg", use_container_width=True)
+st.image("src/images/pypi_package.png")
