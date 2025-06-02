4e387211516b59a3cb8586ddd217d415f1cdec4f
Philipp Meyer <philippmeyer2003@gmail.com>
2025-06-02 14:47:40 +0200

    Swapped frontend image and link

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.page_setup import set_page_layout
 
 set_page_layout(
-    page_title="This is a demopage for the PyPi package ```easy-nlp-translate``` to be found at https://pypi.org/project/easy-nlp-translate/",
+    page_title="This is a demopage for the PyPi package ```easy-nlp-translate``` to be found at https://easy-nlp-translate.de/",
 )
 
 st.write(
